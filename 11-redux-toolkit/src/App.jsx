39ba137569e5d38ba171131b7878c16d3acc4eb3
uzair330 <uzair330@gmail.com>
2024-07-09 13:11:19 +0500

    Todo app with redux tool kit completed

---
+++ 11-redux-toolkit/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="">
+
+    <h1 className="text-xl">Todo App with React tool-kit</h1>
+    <AddTodo/>
+    <Todos/>
+    </div>
+
+  )
+}
+
+export default App
