305cb05f71f8951681c650a0e50d330254f19dbf
uzair330 <uzair330@gmail.com>
2024-07-18 19:24:32 +0500

    login component created

--- 12-blogging-site/src/components/index.js
+++ 12-blogging-site/src/components/index.js
@@ -3,11 +3,17 @@ import Footer from "./Footer";
 import Logo from "./Logo"
 import Container from "./container/Container";
 import LogoutBtn from "./LogoutBtn";
+import Input from './Input'
+import Button from './Button'
+import Select from './Select'
 
 export {
     Header,
     Footer,
     Logo,
     Container,
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    Button,
+    Select
 }
